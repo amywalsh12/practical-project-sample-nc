82b6ca4665a9d62cefaf2bdd0b1e2a79f3e3fb29
Dara Oladapo <daraoladapo@outlook.com>
2021-06-01 11:02:38 +0100

    make it call

--- frontend/Controllers/HomeController.cs
+++ frontend/Controllers/HomeController.cs
@@ -20,18 +20,9 @@ namespace frontend.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var serviceThree = "https://servicethree/merge";
+            var serviceThreeResponseCall = await new HttpClient().GetAsStringAsync(serviceThree);
+            return View(serviceThree);
         }
     }
 }
---
+++ servicethree/Controllers/MergeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace servicethree.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        [HttpGet]
+        public async IActionResult Get()
+        {
+            var serviceOne = "https://serviceone/numbers";
+            var serviceOneResponseCall = await new HttpClient().GetAsStringAsync(serviceOne);
+            var serviceTwo = "https://servicetwo/letters";
+            var serviceTwoResponseCall = await new HttpClient().GetAsStringAsync(serviceTwo);
+            var mergedResponse = $"{serviceOneResponseCall}{serviceTwoResponseCall}";
+            return Ok(mergedResponse);
+        }
+    }
+}
